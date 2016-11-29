cf44ca9b558bd589c47f4d4bde6c9501b44d96af
Sandro Groganz <sandro@campaignchain.com>
2016-11-29 15:04:46 +0100

    CampaignChain/campaignchain#325 "Invalid parameter" warning when creating FB activity with image

--- Job/PublishStatus.php
+++ Job/PublishStatus.php
@@ -166,7 +166,7 @@ class PublishStatus implements JobActionInterface
 
             //Facebook handles only 1 image
             $paramsImg['url'] = $this->cacheManager
-                ->getBrowserPath($images[0]->getPath(), "facebook_photo");
+                ->getBrowserPath($images[0]->getPath(), "campaignchain_facebook_photo");
 
             try {
                 $responseImg = $connection->api('/'.$status->getFacebookLocation()->getIdentifier().'/photos', 'POST', $paramsImg);
